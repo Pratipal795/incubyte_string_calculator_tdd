f82c32d294f5a8a19fd8b1c56465d9df18c684bb
Your Name <you@example.com>
2025-04-03 19:57:37 +0530

    Covered new lines escape sequence string

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -18,5 +18,9 @@ describe StringCalculator do
     it 'returns sum of multiple numbers separated by comma' do
       expect(StringCalculator.add("1,2,3,4")) .to eq(10)
     end
+
+    it 'handles new lines between numbers' do
+      expect(StringCalculator.add("1\n2,3")).to eq(6)
+    end
   end
 end
